34b98a2a72c73ddcb142b016a2826a3f305ec2b9
Iziz-cyber <isispark64@gmail.com>
2025-05-08 14:21:11 -0500

    Add files via upload

---
+++ Inicio/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="Estilos.css">
+</head>
+<body>
+    <div class="container">
+        <nav class="sidebar">
+            <a href="/inicio/">Inicio</a>
+            <a href="/Conceptos/Index2.php">Conceptos</a>
+            <a href="/unirArchivos/">Procesos</a>
+            <a href="/unirArchivos/">Aplicaciones</a>
+            <a href="/Capacidades/nuevoC.php">Capacidades</a>
+        </nav>
+        <main class="content">
+            <div class="cards">
+                <div class="card">
+                    <h2>Conceptos</h2>
+                    <p>Los conceptos representan elementos fundamentales de información que estructuran y organizan el conocimiento dentro de un sistema o dominio específico. Estos pueden ser categorías, definiciones o entidades que tienen atributos y relaciones con otros conceptos.</p>
+                    <a href="/Conceptos/Index2.php" class="button">Conceptos</a>
+                </div>
+                <div class="card">
+                    <h2>Capacidad</h2>
+                    <p>Las capacidades de negocio representan lo que una organización es capaz de hacer para cumplir con sus objetivos estratégicos y operativos. Son elementos fundamentales que describen las competencias y funciones esenciales del negocio, independientemente de cómo se implementen o quién las realice</p>
+                    <a href="/Capacidades/IndexC.php" class="button">Capacidad</a>
+                </div>
+                <div class="card">
+                    <h2>Tipos de negocios</h2>
+                    <p>Gestiona tu inventario con precisión y claridad. Analiza flujos de suministro y optimiza tus decisiones con herramientas visuales avanzadas.</p>
+                    <a href="/abastecimientoClaro/" class="button">Negocios</a>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
